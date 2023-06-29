c769953492ac07d9e3796e592934965c8e0866f2
jrexiti <jay.rexiti@gmail.com>
2023-06-29 12:10:18 -0400

    testng annotations part1

--- src/driverConfig/ChromeDemo.java
+++ src/driverConfig/ChromeDemo.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ChromeDemo {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		
-        System.setProperty("webdriver.chrome.driver", "/Users/jrexiti/Desktop/Resources/drivers_v1/chromedriver");
 
-        WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/Users/jrexiti/Desktop/Resources/drivers_v1/chromedriver");
 
-        driver.manage().window().maximize();;
-        driver.get("https:www.google.com");
-        Thread.sleep(3000);
-        driver.quit();
-		
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		;
+		driver.get("https:www.google.com");
+		Thread.sleep(3000);
+		driver.quit();
+
 	}
 
 }
---
+++ src/package_01/annotations.java
@@ -0,0 +1,38 @@
+package package_01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class annotations {
+
+	WebDriver driver;
+
+	@BeforeSuite
+	public void setup() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/Users/jrexiti/Desktop/Resources/drivers_v1/chromedriver");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("https:www.google.com");
+		Thread.sleep(2000);
+
+	}
+
+	@AfterSuite
+	public void end() {
+
+		driver.close();
+		driver.quit();
+	}
+
+}
