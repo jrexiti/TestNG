2013f29ab40b12f08e8db3e93b5fd0b741727aab
jrexiti <jay.rexiti@gmail.com>
2023-06-29 14:21:44 -0400

    went over passing values from a config file

--- src/testngParams/login.java
+++ src/testngParams/login.java
@@ -1,5 +1,8 @@
 package testngParams;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,26 +12,32 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class login {
-	
 
 	WebDriver driver;
 
 	@BeforeSuite
-	public void setup() throws InterruptedException {
+	public void setup() throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver", "/Users/jrexiti/Desktop/Resources/drivers_v1/chromedriver");
 
 		driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 		Thread.sleep(2000);
+
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(
+				"/Users/jrexiti/Documents/Github/testNG-maven/src/testngParams/config.properties");
+
+		prop.load(data);
+		driver.get(prop.getProperty("url"));
 
 	}
 
 	@Test
 	public void signIn() {
-		driver.get("http://www.teststore.automationtesting.co.uk/");
 		driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
 		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
 		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
