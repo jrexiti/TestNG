b6dcc9786ac74677f86d1632a0ef86efa6c4f41f
jrexiti <jay.rexiti@gmail.com>
2023-06-29 11:47:00 -0400

    execution scope to packages and classes xml file

---
+++ src/package_01/testngXml3.java
@@ -0,0 +1,17 @@
+package package_01;
+
+import org.testng.annotations.Test;
+
+public class testngXml3 {
+
+	@Test
+	public void method5() {
+		System.out.println("method: 5");
+
+	}
+	@Test
+	public void method6() {
+		System.out.println("method: 6");
+
+	}
+}
---
+++ src/package_01/testngXml4.java
@@ -0,0 +1,17 @@
+package package_01;
+
+import org.testng.annotations.Test;
+
+public class testngXml4 {
+
+	@Test
+	public void method7() {
+		System.out.println("method: 7");
+
+	}
+	@Test
+	public void method8() {
+		System.out.println("method: 8");
+
+	}
+}
